36b62737a5fdc0a42f954f3d21d8c8abba261325
Adam Y. Cole II <afewburritos@gmail.com>
2024-08-15 14:27:17 -0500

    add zustand :)

--- src/routes/grades/Grades.tsx
+++ src/routes/grades/Grades.tsx
@@ -1,56 +1,31 @@
 import { useState } from "react";
 import "./grades.css";
 import { FaPlus } from "react-icons/fa";
-
-const fakeData = [
-  {
-    name: "Course A",
-    assignments: [
-      { name: "Assignment 1", grade: 100, portion_of_grade: 0.2, t: false },
-      { name: "Assignment 2", grade: 90, portion_of_grade: 0.1, t: false },
-      { name: "Assignment 3", grade: 0, portion_of_grade: 0.3, t: true },
-    ],
-  },
-  {
-    name: "Course B",
-    assignments: [
-      { name: "Assignment 3", grade: 100, portion_of_grade: 0.2, t: false },
-      { name: "Assignment 4", grade: 9, portion_of_grade: 0.1, t: false },
-      { name: "Assignment 5", grade: -12, portion_of_grade: 0, t: true },
-      { name: "Assignment 4", grade: 9, portion_of_grade: 0.1, t: false },
-      { name: "Assignment 5", grade: -12, portion_of_grade: 0, t: true },
-      { name: "Assignment 4", grade: 9, portion_of_grade: 0.1, t: false },
-      { name: "Assignment 5", grade: -12, portion_of_grade: 0, t: true },
-      { name: "Assignment 4", grade: 9, portion_of_grade: 0.1, t: false },
-      { name: "Assignment 5", grade: -12, portion_of_grade: 0, t: true },
-      { name: "Assignment 4", grade: 9, portion_of_grade: 0.1, t: false },
-      { name: "Assignment 5", grade: -12, portion_of_grade: 0, t: true },
-      { name: "Assignment 4", grade: 9, portion_of_grade: 0.1, t: false },
-      { name: "Assignment 5", grade: -12, portion_of_grade: 0, t: true },
-      { name: "Assignment 4", grade: 9, portion_of_grade: 0.1, t: false },
-      { name: "Assignment 5", grade: -12, portion_of_grade: 0, t: true },
-    ],
-  },
-];
+import { produce } from "immer";
+import { useGWState, Course, Assignment } from "../state_hook";
 
 export default function Grades() {
-  const [currentSelectedCourse, setCurrentSelectedCourse] = useState(
-    fakeData[0]
+  const courses = useGWState((state: { courses: Course[] }) => state.courses);
+  const setCourseState = useGWState(
+    (state: { setCourseState: (courses: Course[]) => void }) =>
+      state.setCourseState
   );
+  const [currentSelectedCourse, setCurrentSelectedCourse] = useState(0);
   return (
     <div className="grades">
       <span className="selected-course-wrapper">
         <select
           className="selected-course"
-          value={currentSelectedCourse.name}
+          value={courses[currentSelectedCourse].name}
           onChange={(e) =>
             setCurrentSelectedCourse(
-              fakeData.find((course) => course.name === e.target.value) ??
-                fakeData[0]
+              courses.findIndex(
+                (course: Course) => course.name === e.target.value
+              )
             )
           }
         >
-          {fakeData.map((course) => (
+          {courses.map((course: Course) => (
             <option key={course.name} value={course.name}>
               {course.name}
             </option>
@@ -67,79 +42,74 @@ export default function Grades() {
             </tr>
           </thead>
           <tbody>
-            {currentSelectedCourse.assignments.map((assignment, index) => (
-              <tr key={index}>
-                <td>
-                  <input
-                    className="grades-input"
-                    type="text"
-                    value={assignment.name}
-                    onChange={(e) => {
-                      const newAssignments = [
-                        ...currentSelectedCourse.assignments,
-                      ];
-                      newAssignments[index].name = e.target.value;
-                      setCurrentSelectedCourse({
-                        ...currentSelectedCourse,
-                        assignments: newAssignments,
-                      });
-                    }}
-                  />
-                </td>
-                <td>
-                  <input
-                    className="grades-input"
-                    type="number"
-                    value={assignment.grade}
-                    onChange={(e) => {
-                      const newAssignments = [
-                        ...currentSelectedCourse.assignments,
-                      ];
-                      newAssignments[index].grade = parseInt(e.target.value);
-                      setCurrentSelectedCourse({
-                        ...currentSelectedCourse,
-                        assignments: newAssignments,
-                      });
-                    }}
-                  />
-                </td>
-                <td>
-                  <input
-                    className="grades-input"
-                    type="number"
-                    value={assignment.portion_of_grade}
-                    onChange={(e) => {
-                      const newAssignments = [
-                        ...currentSelectedCourse.assignments,
-                      ];
-                      newAssignments[index].portion_of_grade = parseFloat(
-                        e.target.value
-                      );
-                      setCurrentSelectedCourse({
-                        ...currentSelectedCourse,
-                        assignments: newAssignments,
-                      });
-                    }}
-                  />
-                </td>
-              </tr>
-            ))}
+            {courses[currentSelectedCourse].assignments.map(
+              (assignment: Assignment, index: number) => (
+                <tr key={index}>
+                  <td>
+                    <input
+                      className="grades-input"
+                      type="text"
+                      value={assignment.name}
+                      onChange={(e) => {
+                        setCourseState(
+                          produce(courses, (draft) => {
+                            draft[currentSelectedCourse].assignments[
+                              index
+                            ].name = e.target.value;
+                          })
+                        );
+                      }}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      className="grades-input"
+                      type="number"
+                      value={assignment.grade}
+                      onChange={(e) => {
+                        setCourseState(
+                          produce(courses, (draft) => {
+                            draft[currentSelectedCourse].assignments[
+                              index
+                            ].grade = parseFloat(e.target.value);
+                          })
+                        );
+                      }}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      className="grades-input"
+                      type="number"
+                      value={assignment.weight}
+                      onChange={(e) => {
+                        setCourseState(
+                          produce(courses, (draft) => {
+                            draft[currentSelectedCourse].assignments[
+                              index
+                            ].weight = parseFloat(e.target.value);
+                          })
+                        );
+                      }}
+                    />
+                  </td>
+                </tr>
+              )
+            )}
           </tbody>
         </table>
         <button
           className="add-assignment"
           onClick={() => {
-            const newAssignments = [...currentSelectedCourse.assignments];
-            newAssignments.push({
-              name: "",
-              grade: 0,
-              portion_of_grade: 0,
-              t: false,
-            });
-            setCurrentSelectedCourse({
-              ...currentSelectedCourse,
-              assignments: newAssignments,
-            });
+            setCourseState(
+              produce(courses, (draft) => {
+                draft[currentSelectedCourse].assignments.push({
+                  name: "",
+                  grade: 0,
+                  weight: 0,
+                });
+              })
+            );
           }}
         >
           <FaPlus />
